1916c75f5d3c8fd9c2d6958c6e8eb5e840bfa7a1
delvoye <benjamin.delvoye@student.uliege.be>
2020-04-16 22:06:21 +0200

    new model

--- 05_MILESTONES/Europe/aggregated/Europe_model.py
+++ 05_MILESTONES/Europe/aggregated/Europe_model.py
@@ -2,7 +2,9 @@ import pandas as pd
 import numpy as np
 import pickle
 
+import matplotlib
 import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error
 from sklearn.kernel_ridge import KernelRidge
 
 from sklearn.preprocessing import PolynomialFeatures
@@ -26,17 +28,16 @@ df = pd.read_csv('EU28_dataconcat.csv', index_col=0)
 # #plt.show()
 
 # Create train and validation sets
-X = df[['pop', 'gdpc', 'nrg_int', 'co2_int']].to_numpy()
+# X = df[['pop', 'gdpc', 'nrg_int', 'co2_int']].to_numpy()
+X = df[['pop', 'gdpc', 'nrg_int', 'est_ci']].to_numpy()
 y = df['co2'].to_numpy()
 
-nbr_train = 16
+nbr_train = 20
 y_train = y[:nbr_train]
 y_val = y[nbr_train:]
 
 X_train = X[:nbr_train, :]
 X_val = X[nbr_train:, :]
-kaya_val = X_val.prod(axis=1)
-kaya_val = np.insert(kaya_val, 0, y_train[-1])
 
 # prepare model
 model = make_pipeline(StandardScaler(), GridSearchCV(KernelRidge(kernel='rbf', gamma=0.1), cv=5, iid=True,
@@ -49,14 +50,27 @@ pickle.dump(model, open(filename, 'wb'))
 
 pred_X = fitted.predict(X)
 pred_val = fitted.predict(X_val)
-pred_val = np.insert(pred_val, 0, y_train[-1])
+
+rmse = mean_squared_error(y_val, pred_val, squared=False)
+print('RMSE: {}'.format(rmse))
+pred_val = np.insert(pred_val, 0, y_train[-1])/10**6
 
 # plot results
-fig, ax = plt.subplots(figsize=(20, 10))
-ax.plot(df.index, y)
-ax.plot(np.arange((2019 - len(pred_val)), 2019), pred_val)
-ax.plot(df.index, pred_X)
-# ax.plot(np.arange((2019-len(pred_val)), 2019), kaya_val)
-plt.xticks(np.arange(1990, 2019))
+size = 15
+params = {'legend.fontsize': size*1.5,
+          'axes.labelsize': size*1.5,
+          'xtick.labelsize': size*0.75,
+          'ytick.labelsize': size*0.75}
+matplotlib.rcParams.update(params)
 
-plt.show()
+fig, ax = plt.subplots(figsize=(20, 10))
+ax.plot(df.index, y/10**6, label='Target emissions')
+ax.plot(np.arange((2019 - len(pred_val)), 2019), pred_val, label='Predicted emissions')
+
+plt.xticks(np.arange(1990, 2019))
+plt.xlabel('Years')
+plt.ylabel('CO2 emissions [million tonne]')
+plt.legend()
+
+plt.savefig('model_emissions.png')
+plt.close()
--- 05_MILESTONES/Europe/aggregated/Europe_preprocess.py
+++ 05_MILESTONES/Europe/aggregated/Europe_preprocess.py
@@ -26,6 +26,30 @@ nrg.set_index('TIME', inplace=True)
 nrg = nrg['Value'].str.replace(",", "").astype(float) * 1000
 print(nrg)
 
+fuels = pd.read_csv('fuels_mix.csv', index_col='YEAR')
+
+ng = fuels['natural_gas'].str.replace(",", ".").astype(float)
+ng = ng.transpose()
+ng[2018] = 2*ng[2017]-ng[2016]
+ng = ng.transpose()
+
+coal = fuels['solid_fossil'].str.replace(",", ".").astype(float)
+coal = coal.transpose()
+coal[2018] = 2*coal[2017]-coal[2016]
+coal = coal.transpose()
+
+oil = fuels['oil'].str.replace(",", ".").astype(float)
+oil = oil.transpose()
+oil[2018] = 2*oil[2017]-oil[2016]
+oil = oil.transpose()
+
+#transform consumption into CO2
+ng = 2.236*ng
+coal = 4.019*coal
+oil = 2.293*oil
+est_ci = (ng+coal+oil).values/nrg.values
+est_ci = est_ci* 10**6
+est_ci = pd.DataFrame({'est_ci': est_ci}, index=pop.index)
 co2 = pd.read_csv('fossilCO2_emissions_edgar.csv', skiprows=1)
 co2.set_index('date', inplace=True)
 co2 = co2.iloc[20:]
@@ -57,7 +81,8 @@ var18 = [pop18, gdpc18, nrg_int18, co2_int18]
 var18 = pd.DataFrame({'pop': [var18[0]], 'gdpc': [var18[1]], 'nrgint': [var18[2]], 'co2int': [var18[3]]}, index=[2018])
 var18.to_csv('var18.csv')
 
-df = [pop, gdpc, nrg_int, co2_int, co2]
+# df = [pop, gdpc, nrg_int, co2_int, co2]
+df = [pop, gdpc, nrg_int, est_ci, co2]
 df = pd.concat(df, axis=1, sort=False, join='inner')
 print(df)
 
